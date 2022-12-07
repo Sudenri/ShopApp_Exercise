78c98c6d4826f5a39bb187fb43b1f82314b52784
Sudenri <tsui_stanley@hotmail.com>
2022-12-08 00:16:46 +0100

    Completion Exercise 4.3

--- src/duke/choice/ShopApp.java
+++ src/duke/choice/ShopApp.java
@@ -6,7 +6,7 @@ public class ShopApp {
 
     public static void main(String[] args) {
         double tax = 0.2;
-        double total;
+        double total = 0.0;
         int measurement = 3;
 
         Customer c1 = new Customer();
@@ -16,11 +16,14 @@ public class ShopApp {
         Clothing item1 = new Clothing("Blue Jacket", 20.9,"M" );
         Clothing item2 = new Clothing("Orange T-Shirt", 10.5, "S");
         Clothing[] items = {item1, item2};
-        total = (1 * item1.price + 2 * item2.price) * (1 + tax);
 
         System.out.println("Hi " + c1.name + ", Welcome to Duke Choice Shop!");
-        System.out.println("Item 1" + ", " + item1.description + ", " + item1.size + ", " + item1.price);
-        System.out.println("Item 2" + ", " + item2.description + ", " + item2.size + ", " + item2.price);
+
+        for(Clothing item: items){
+            System.out.println("The selected item is a " + item.description + "of size " + "and costs " + item.price);
+            total = total + item.price + item.price * tax;
+        }
+
         System.out.println("The total you need to pay is " + total);
 
         switch (measurement) {
